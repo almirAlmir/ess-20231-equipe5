038b065a0cf2a5ec0ce41c23a6310557b3f5bbae
almirAlmir <aasc@cin.ufpe.br>
2023-08-07 21:59:27 -0300

    Criação da

---
+++ backend/src/repo/anime_repo.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from src.models import anime_model
+from src.schemas import anime_schema
+
+class RepositorioAnimes():
+    
+    def __init__(self, db: Session):
+        self.db = db
+        
+    def criar(self, anime: anime_schema.Anime):
+        db_anime = anime_model.Anime(nome_anime=anime.nome_anime,
+                                     qtd_assitido=anime.qtd_assistido,
+                                     likes=anime.likes)
+        
+        self.db.add(db_anime)
+        self.db.commit()
+        self.db.refresh()
+        
+        return db_anime
